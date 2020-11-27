42d883f12298c3d2f6f23a9af50ed9b131db7f5a
HendrikWeisgerber <57496555+HendrikWeisgerber@users.noreply.github.com>
2020-11-27 17:30:33 +0100

    swopping to VS Code

---
+++ HelloWorld/src/main/java/com/example/lib/Bestellung.java
@@ -0,0 +1,19 @@
+package com.example.lib;
+
+public class Bestellung {
+    private String vorname;
+    private String nachname;
+    private String username;
+    private int id;
+    private int alter;
+    private String email;
+    private String passwortHash;
+    //private Rechte rechte;
+    private Boolean newsletter;
+    //private Preiskategorie preiskategorie;
+    private Film wunschliste;
+    //private Zahlungsmethode zahlungsmethode;
+    //private Zone lieblingszone;
+    private Warenkorb warenkorb;
+
+}
