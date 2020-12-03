5103b868c13ebfbbcb069255f2156016fe0c7ce9
Schridde <moritz.schridde@sap.com>
2020-12-03 11:27:37 +0100

    lettercase fix

--- HelloWorld/src/main/java/com/example/lib/Repositories/warenkorbRepository.java
+++ HelloWorld/src/main/java/com/example/lib/Repositories/warenkorbRepository.java
@@ -3,5 +3,5 @@ package com.example.lib.Repositories;
 import com.example.lib.Warenkorb;
 import org.springframework.data.repository.CrudRepository;
 
-public interface warenkorbRepository extends CrudRepository<Warenkorb, Integer> {
+public interface WarenkorbRepository extends CrudRepository<Warenkorb, Integer> {
 }
