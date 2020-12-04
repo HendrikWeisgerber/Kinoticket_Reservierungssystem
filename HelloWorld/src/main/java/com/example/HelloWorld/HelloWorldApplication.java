3aad65d4639a5334e01ca1bc1d3c90571a028aa8
I517970 <niklas.schaefer@sap.com>
2020-12-04 12:56:33 +0100

    Update HelloWorldApplication.java

--- HelloWorld/src/main/java/com/example/HelloWorld/HelloWorldApplication.java
+++ HelloWorld/src/main/java/com/example/HelloWorld/HelloWorldApplication.java
@@ -20,47 +20,65 @@ import com.example.lib.Vorstellung;
 @SpringBootApplication
 @RestController
 public class HelloWorldApplication {
-	private static Map<Integer, Film> filme = new HashMap<>();
-	private static Map<Integer, Vorstellung> vorstellungen = new HashMap<>();
+	private static Map<Integer, Film> aktFilme = new HashMap<Integer, Film>();
+	private static Map<Integer, Film> deaktFilme = new HashMap<Integer, Film>();
 	static{
-		Film starWars = new Film("Star Wars", 1, "the picture is here", "Its cool", 10, 140, 12, true, null);
-		Film starWars2 = new Film("Star Wars2", 2, "the picture is here", "Its cool", 10, 140, 12, true, null);
-		filme.put(starWars.getId(),starWars);
-		filme.put(starWars2.getId(),starWars2);
+		Film starWars = new Film("Star Wars", 1, "the picture is here", "Its cool", 9, 140, 12, true, null);
+		Film starWars2 = new Film("Star Wars 2", 2, "the picture is here", "Its cool", 8, 140, 12, true, null);
+		Film starWars6 = new Film("Star Wars 6", 3, "the picture is here", "Its cool", 10, 140, 12, false, null);
+		Film starWars3 = new Film("Star Wars 3", 4, "the picture is here", "Its cool", 10, 140, 12, true, null);
+		aktFilme.put(starWars.getId(),starWars);
+		aktFilme.put(starWars2.getId(),starWars2);
+		aktFilme.put(starWars6.getId(),starWars6);
+		aktFilme.put(starWars3.getId(),starWars3);
+
+		for(int i=0; i <= aktFilme.size(); i++) {
+			if(aktFilme.get(i) != null){
+				if(!aktFilme.get(i).getAktiv()) {
+					deaktFilme.put(i, aktFilme.get(i));
+					aktFilme.remove(i);
+				}
+			}
+		}
 	}
 //Alle Http-Anfragen zu Film
 	@RequestMapping(value = "/", produces = "application/json")
 	public ResponseEntity<Object> getFilme(){
-		return new ResponseEntity<>(filme, HttpStatus.OK);
+		return new ResponseEntity<>(aktFilme, HttpStatus.OK);
 	}
+	//Get deaktFilme
+	@RequestMapping(value = "/film/false")
+   	public ResponseEntity<Object> getProduct() {
+      return new ResponseEntity<>(deaktFilme.values(), HttpStatus.OK);
+   }
 	//PUT
 	@RequestMapping(value = "/film/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Object> updateFilm(@PathVariable("id") Integer id, @RequestBody Film film) { 
-		filme.remove(id);
+		aktFilme.remove(id);
 		film.setId(id);
-		filme.put(id, film);
+		aktFilme.put(id, film);
 	   return new ResponseEntity<>("Product is updated successsfully", HttpStatus.OK);
 	}   
 	//POST
 	@RequestMapping(value = "/film", method = RequestMethod.POST)
    public ResponseEntity<Object> postFilm(@RequestBody Film film) {
-      filme.put(film.getId(), film);
+      aktFilme.put(film.getId(), film);
       return new ResponseEntity<>("Product is created successfully", HttpStatus.CREATED);
    }
    //DEACTIVATE
    @RequestMapping(value = "/film/{id}", method = RequestMethod.DELETE)
    public ResponseEntity<Object> deleteFilm(@PathVariable("id") Integer id) { 
-      filme.get(id).setAktiv(false);;
+      aktFilme.get(id).setAktiv(false);;
       return new ResponseEntity<>("Product is deleted successsfully", HttpStatus.OK);
    }
 //Alle Http Anfragen zu Vorstellungen
 	@RequestMapping(value = "/vorstellung", method = RequestMethod.POST)
 	public ResponseEntity<Object> postVorstellung(@RequestBody Vorstellung vorstellung) {
-		Film filmAlt = filme.get(vorstellung.getFilmId());
+		Film filmAlt = aktFilme.get(vorstellung.getFilmId());
 		filmAlt.addVorstellung(vorstellung);
-		filme.remove(vorstellung.getFilmId());
+		aktFilme.remove(vorstellung.getFilmId());
 		//filmAlt.setId(vorstellung.getFilmId());
-		filme.put(vorstellung.getFilmId(), filmAlt);
+		aktFilme.put(vorstellung.getFilmId(), filmAlt);
 	return new ResponseEntity<>("Product is created successfully", HttpStatus.CREATED);
 	}
 	public static void main(String[] args) {
