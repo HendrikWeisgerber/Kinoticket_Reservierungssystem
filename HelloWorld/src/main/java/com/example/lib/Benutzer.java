c3a8d0fb6a045254a6e66a3b304502370fd82c5a
Schridde <moritz.schridde@sap.com>
2021-01-06 14:50:37 +0100

    implemented Preiskategorie Enum

--- HelloWorld/src/main/java/com/example/lib/Benutzer.java
+++ HelloWorld/src/main/java/com/example/lib/Benutzer.java
@@ -1,6 +1,10 @@
 package com.example.lib;
 
+import java.math.BigDecimal;
+
 import javax.persistence.*;
+
+import com.example.lib.Enum.Preiskategorie;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -23,12 +27,13 @@ public class Benutzer {
     private Film[] wunschliste;
     private Boolean newsletter;
     //private Rechte rechte;
-    //private Preiskategorie preiskategorie;
+    private Preiskategorie preiskategorie;
+
     //private Zahlungsmethode zahlungsmethode;
     //private Zone lieblingszone;
 
     public Benutzer(String vorname, String nachname, String username, int id, int alter, String email,
-                    String passwortHash, Warenkorb warenkorb, Film[] wunschliste, Boolean newsletter) {
+    String passwortHash, Warenkorb warenkorb, Film[] wunschliste, Boolean newsletter) {
         this.vorname = vorname;
         this.nachname = nachname;
         this.username = username;
@@ -39,10 +44,26 @@ public class Benutzer {
         this.warenkorb = warenkorb;
         this.wunschliste = wunschliste;
         this.newsletter = newsletter;
+        this.preiskategorie = Preiskategorie.ERWACHSENER;
     }
 
     public Benutzer(String vorname, String nachname, String username, int id, int alter, String email,
-                    String passwortHash, Warenkorb warenkorb, Boolean newsletter) {
+    String passwortHash, Warenkorb warenkorb, Film[] wunschliste, Boolean newsletter, Preiskategorie preiskategorie) {
+        this.vorname = vorname;
+        this.nachname = nachname;
+        this.username = username;
+        this.id = id;
+        this.alter = alter;
+        this.email = email;
+        this.passwortHash = passwortHash;
+        this.warenkorb = warenkorb;
+        this.wunschliste = wunschliste;
+        this.newsletter = newsletter;
+        this.preiskategorie = preiskategorie;
+    }
+
+    public Benutzer(String vorname, String nachname, String username, int id, int alter, String email,
+    String passwortHash, Warenkorb warenkorb, Boolean newsletter) {
         this.vorname = vorname;
         this.nachname = nachname;
         this.username = username;
@@ -53,85 +74,85 @@ public class Benutzer {
         this.warenkorb = warenkorb;
         this.newsletter = newsletter;
     }
-
+    
     @Autowired
     public Benutzer() {
-
+        
     }
-
-
+    
+    
     public String getVorname() {
         return this.vorname;
     }
-
+    
     public void setVorname(String vorname) {
         this.vorname = vorname;
     }
-
+    
     public String getNachname() {
         return this.nachname;
     }
-
+    
     public void setNachname(String nachname) {
         this.nachname = nachname;
     }
-
+    
     public String getUsername() {
         return this.username;
     }
-
+    
     public void setUsername(String username) {
         this.username = username;
     }
-
+    
     public int getId() {
         return this.id;
     }
-
+    
     public void setId(int id) {
         this.id = id;
     }
-
+    
     public int getAlter() {
         return this.alter;
     }
-
+    
     public void setAlter(int alter) {
         this.alter = alter;
     }
-
+    
     public String getEmail() {
         return this.email;
     }
-
+    
     public void setEmail(String email) {
         this.email = email;
     }
-
+    
     public String getPasswortHash() {
         return this.passwortHash;
     }
-
+    
     public void setPasswortHash(String passwortHash) {
         this.passwortHash = passwortHash;
     }
-
+    
     public Warenkorb getWarenkorb() {
         return this.warenkorb;
     }
-
+    
     public void setWarenkorb(Warenkorb warenkorb) {
         this.warenkorb = warenkorb;
     }
-
+    
     public Film[] getWunschliste() {
         return this.wunschliste;
     }
-
+    
     public void setWunschliste(Film[] wunschliste) {
         this.wunschliste = wunschliste;
     }
-
+    
     public void derWunschlisteHinzufuegen(Film film) {
         Film[] neueWunschListe = new Film[this.wunschliste.length + 1];
         int i = 0;
@@ -142,19 +163,44 @@ public class Benutzer {
         neueWunschListe[i] = film;
         this.wunschliste = neueWunschListe;
     }
-
+    
     public Boolean getNewsletter() {
         return this.newsletter;
     }
-
+    
     public void setNewsletter(Boolean newsletter) {
         this.newsletter = newsletter;
+    }
+    
+    public Preiskategorie getPreiskategorie() {
+        return this.preiskategorie;
+    }
+    
+    public void setPreiskategorie(Preiskategorie preiskategorie) {
+        this.preiskategorie = preiskategorie;
+    }
+
+    public BigDecimal getPreisSchluessel(){
+        switch (this.preiskategorie){
+            case STUDIEREND:
+                return new BigDecimal(0.8);
+            case KIND:
+                return new BigDecimal(0.6);
+            case SENIOR:
+                return new BigDecimal(0.7);
+            case MENSCH_MIT_BEHINDERUNG:
+                return new BigDecimal(0.5);
+            case BEGLEITPERSON:
+                return new BigDecimal(0);
+            default:
+                return new BigDecimal(1.0);
+        }
     }
 
     public boolean istRichtigesPasswort(String passwort){
         return ((Integer)passwort.hashCode()).toString().equals(passwortHash) ;
     }
-//TODO Preiskategorien bestimmen und Rechte 
-
-
+    //TODO Rechte 
+    
+    
 }
