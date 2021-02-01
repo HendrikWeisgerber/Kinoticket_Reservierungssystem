3cc1a4d9ceabb88470b8a0ab4a3babff4c586df4
Schridde <moritz.schridde@sap.com>
2021-02-01 17:17:17 +0100

    film controller tests

--- HelloWorld/src/main/java/com/example/lib/controller/FilmController.java
+++ HelloWorld/src/main/java/com/example/lib/controller/FilmController.java
@@ -127,108 +127,70 @@ public class FilmController {
 
         for (String genreName : genreArray) {
             if (genreCounter < genres.length) {
-                switch (genreName) { // TODO besser toLowerCase() verwenden, wäre auch weniger Arbeit XD
-                    case "Action":
-                    case "ACTION":
+                switch (genreName.toLowerCase()) { // TODO besser toLowerCase() verwenden, wäre auch weniger Arbeit XD
                     case "action":
                         genres[genreCounter] = Genre.ACTION;
                         genreCounter++;
                         break;
-                    case "Comedy":
-                    case "COMEDY":
                     case "comedy":
                         genres[genreCounter] = Genre.COMEDY;
                         genreCounter++;
                         break;
-                    case "Horror":
-                    case "HORROR":
                     case "horror":
                         genres[genreCounter] = Genre.HORROR;
                         genreCounter++;
                         break;
-                    case "Sci-Fi":
-                    case "SCI-FI":
                     case "sci-fi":
-                    case "Sci_Fi":
-                    case "SCI_FI":
                     case "sci_fi":
-                    case "SciFi":
-                    case "SCIFI":
                     case "scifi":
                         genres[genreCounter] = Genre.SCI_FI;
                         genreCounter++;
                         break;
-                    case "Thriller":
-                    case "THRILLER":
                     case "thriller":
                         genres[genreCounter] = Genre.THRILLER;
                         genreCounter++;
                         break;
-                    case "Fantasy":
-                    case "FANTASY":
                     case "fantasy":
                         genres[genreCounter] = Genre.FANTASY;
                         genreCounter++;
                         break;
-                    case "Drama":
-                    case "DRAMA":
                     case "drama":
                         genres[genreCounter] = Genre.DRAMA;
                         genreCounter++;
                         break;
-                    case "Documentary":
-                    case "DOCUMENTARY":
                     case "documentary":
                         genres[genreCounter] = Genre.DOCUMENTARY;
                         genreCounter++;
                         break;
-                    case "History":
-                    case "HISTORY":
                     case "history":
                         genres[genreCounter] = Genre.HISTORY;
                         genreCounter++;
                         break;
-                    case "Animation":
-                    case "ANIMATION":
                     case "animation":
                         genres[genreCounter] = Genre.ANIMATION;
                         genreCounter++;
                         break;
-                    case "Family":
-                    case "FAMILY":
                     case "family":
                         genres[genreCounter] = Genre.FAMILY;
                         genreCounter++;
                         break;
-                    case "Musical":
-                    case "MUSICAL":
                     case "musical":
-                    case "Music":
-                    case "MUSIC":
                     case "music":
                         genres[genreCounter] = Genre.MUSICAL;
                         genreCounter++;
                         break;
-                    case "Short":
-                    case "SHORT":
                     case "short":
                         genres[genreCounter] = Genre.SHORT;
                         genreCounter++;
                         break;
-                    case "Romance":
-                    case "ROMANCE":
                     case "romance":
                         genres[genreCounter] = Genre.ROMANCE;
                         genreCounter++;
                         break;
-                    case "Superhero":
-                    case "SUPERHERO":
                     case "superhero":
                         genres[genreCounter] = Genre.SUPERHERO;
                         genreCounter++;
                         break;
-                    case "Western":
-                    case "WESTERN":
                     case "western":
                         genres[genreCounter] = Genre.WESTERN;
                         genreCounter++;
--- HelloWorld/src/test/java/com/example/HelloWorld/HelloWorldApplicationTests.java
+++ HelloWorld/src/test/java/com/example/HelloWorld/HelloWorldApplicationTests.java
@@ -1,13 +1,77 @@
 package com.example.HelloWorld;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+import java.util.ArrayList;
+
+import com.example.lib.Film;
+import com.example.lib.Repositories.FilmRepository;
+
+import static org.hamcrest.Matchers.containsString;
+
+@SpringBootTest(classes = { com.example.lib.HelloWorldApplication.class })
+@AutoConfigureMockMvc
 class HelloWorldApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private FilmRepository filmrepository;
 
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	public void getAllFilms() throws Exception {
+		// this.mockMvc.perform(MockMvcRequestBuilders.get("/bazahlen/bestellung/151/iban/DE-12345678901234567890"))
+		ArrayList<String> filmNamen = new ArrayList<String>();
+		long size = filmrepository.count();
+		filmNamen.add("name");
+		filmrepository.findAll().forEach(f -> {
+			filmNamen.add(f.getName());
+		});
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/film/all")).andDo(print()).andExpect(status().isOk())
+				.andExpect(content().string(containsString(filmNamen.get((int) (Math.random() * size)))))
+				.andExpect(content().string(containsString(filmNamen.get((int) (Math.random() * size)))))
+				.andExpect(content().string(containsString(filmNamen.get((int) (Math.random() * size)))))
+				.andExpect(content().string(containsString(filmNamen.get((int) (Math.random() * size)))))
+				.andExpect(content().string(containsString(filmNamen.get((int) (Math.random() * size)))))
+				.andExpect(content().string(containsString(filmNamen.get((int) (Math.random() * size)))))
+				.andExpect(content().string(containsString(filmNamen.get((int) (Math.random() * size)))));
+	}
+
+	@Test
+	public void getAFilm() throws Exception {
+		// this.mockMvc.perform(MockMvcRequestBuilders.get("/bazahlen/bestellung/151/iban/DE-12345678901234567890"))
+		ArrayList<Film> filme = new ArrayList<Film>();
+		filmrepository.findAll().forEach(f -> {
+			filme.add(f);
+		});
+		filme.stream().forEach(f -> {
+			String call = "/film/" + f.getId();
+			try {
+				mockMvc.perform(MockMvcRequestBuilders.get(call)).andDo(print()).andExpect(status().isOk())
+						.andExpect(content().string(containsString(f.getName())))
+						.andExpect(content().string(containsString(f.getBeschreibung())))
+						.andExpect(content().string(containsString(((Integer)f.getBewertung()).toString())))
+						.andExpect(content().string(containsString(f.getGenre1().toString())));
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		});
+
+	}
+	
 
 }
